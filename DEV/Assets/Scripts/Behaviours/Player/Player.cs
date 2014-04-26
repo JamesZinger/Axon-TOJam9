8e0278d86bbaedfef1a7a61034a822c7e8841988
James Zinger <dev@jameszinger.com>
2014-04-26 03:26:44 -0400

    Fixed the scrolling background

--- DEV/Assets/Scripts/Behaviours/Player/Player.cs
+++ DEV/Assets/Scripts/Behaviours/Player/Player.cs
@@ -133,7 +133,7 @@ public class Player : MonoBehaviour
 
 	IEnumerator CheckIfGrounded()
 	{
-		yield return new WaitForSeconds(0.5f);
+		yield return new WaitForSeconds(0.1f);
 		while ( true )
 		{
 			Vector2 origin = new Vector2( transform.position.x, transform.position.y );
